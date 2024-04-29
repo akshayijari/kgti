55bec59c06742162cb4faf1eb280d3ccd8e09bb4
akshayijari <akshayijari@gmail.com>
2024-04-29 22:46:58 +0530

    Deploy to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
 
+// export default function ButtonUsage() {
+//   return <Button variant="contained">Hello world</Button>;
+// }
 function App() {
   return (
     <div className="App">
@@ -10,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="contained">Hello world</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
